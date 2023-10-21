117b72d94efb213925701e8f277870a9e8c1d9d0
ranyery <ranyerysc@gmail.com>
2023-10-21 17:37:41 -0300

    chore: update server main.ts

--- server/main.ts
+++ server/main.ts
@@ -1,11 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env['PORT'] || 4000;
+
   app.setGlobalPrefix('api');
-  await app.listen(process.env['PORT'] || 4000);
+  app.enableShutdownHooks();
+  app.getHttpAdapter().getInstance().disable('x-powered-by');
+
+  await app.listen(PORT);
+  Logger.log(`🚀 Application is running on: http://localhost:${PORT}`);
 }
 
 // Webpack will replace 'require' with '__webpack_require__'
