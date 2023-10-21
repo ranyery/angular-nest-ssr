13d48349fb7b52b4f0f8d0d15b86c1a7b9aedbe4
ranyery <ranyerysc@gmail.com>
2023-10-21 11:09:33 -0300

    deps: add @nestjs/ng-universal

---
+++ server.ts
@@ -0,0 +1,4 @@
+import 'zone.js/node';
+import './server/main';
+
+export * from './src/main.server';
---
+++ server/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AngularUniversalModule } from '@nestjs/ng-universal';
+import { join } from 'path';
+
+import { AppServerModule } from '../src/main.server';
+
+@Module({
+  imports: [
+    AngularUniversalModule.forRoot({
+      bootstrap: AppServerModule,
+      viewsPath: join(process.cwd(), 'dist/angular-nest-ssr/browser'),
+    }),
+  ],
+})
+export class AppModule {}
