520c4286936ef6c3198ca6a782fc45ac2cad82e8
ranyery <ranyerysc@gmail.com>
2023-10-21 11:53:52 -0300

    feat: add initial home page module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
-      initialNavigation: 'enabledBlocking',
-    }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
   ],
   exports: [RouterModule],
 })
